de3a5129aa9122a66be22806a772cf5b331b5048
Renato <rrodriguez@outlook.com.br>
2016-11-23 09:46:05 -0200

    Insert project

---
+++ src/main/java/io/spring/guides/gs_producing_web_service/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2016.11.23 às 09:16:58 AM BRST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package io.spring.guides.gs_producing_web_service;
